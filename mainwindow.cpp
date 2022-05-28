6f4d6c938adb5f4e0a087a7531cbf111aae339ca
ecristal <e.cristaldo27@gmail.com>
2022-05-28 09:07:24 -0400

    bump to version v1_02_00

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -58,7 +58,7 @@ void MainWindow::initializeWindow(){
     ui->spinBoxBaudRate->setValue(115200);
     this->serialPort_ = new QSerialPort(this);
     this->dialogReadoutChannelWindow = new DialogReadoutChannel();
-    this->Message("DAPHNE GUI V1_01_12\nAuthor: Ing. Esteban Cristaldo, MSc",0);
+    this->Message("DAPHNE GUI V1_02_00\nAuthor: Ing. Esteban Cristaldo, MSc",0);
 }
 
 void MainWindow::populateComboBoxAvailableSerialPorts(){
