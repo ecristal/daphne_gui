975578690ebeb070036ad3b4d12030de3d7c2012
Esteban Cristaldo <e.cristaldo27@gmail.com>
2023-04-26 13:58:03 +0200

    windows fork; GUI with correct dimensions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
+    a.setStyle(QStyleFactory::create("Fusion"));
     MainWindow w;
     w.show();
     return a.exec();
